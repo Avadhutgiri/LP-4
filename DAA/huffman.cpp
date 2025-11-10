f5963f711d8f14f4447130fcffb8816323e90447
Avadhut Giri <125117822+Avadhutgiri@users.noreply.github.com>
2025-11-10 07:26:47 +0530

    Updated

--- DAA/huffman.cpp
+++ DAA/huffman.cpp
@@ -1,98 +1,81 @@
-#include <iostream>
-#include <queue>
-#include <vector>
-#include <unordered_map>
+/**
+ * Problem Statement: Write a program to implement Huffman Encoding using a greedy strategy.
+ * Must Watch: https://youtu.be/uDS8AkTAcIU?si=J79pJbEHLW588mcA
+ */
+  
+
+#include<bits/stdc++.h>
 using namespace std;
 
-struct HuffmanNode {
-    char data;                  // Input character
-    int frequency;              // Frequency of the character
-    HuffmanNode *left, *right;  // Left and right children
+struct MinHeapNode{
+    char data;  // character
+    int freq;  // freuency of the character
+    MinHeapNode *left, *right; 
 
-    HuffmanNode(char data, int frequency) {
+
+    MinHeapNode(char data, int freq){
         left = right = nullptr;
         this->data = data;
-        this->frequency = frequency;
+        this->freq = freq;
     }
 };
 
-// Comparator to order the min-heap (priority queue)
-struct Compare {
-    bool operator()(HuffmanNode* left, HuffmanNode* right) {
-        return left->frequency > right->frequency;
+struct compare {
+    bool operator()(struct MinHeapNode* left, MinHeapNode* right){
+        return (left->freq > right->freq);
     }
 };
 
-// Function to print the Huffman codes
-void printCodes(HuffmanNode* root, string code, unordered_map<char, string>& huffmanCodes) {
-    if (!root) return;
+// Function to print HuffMan Tree() (hint: preorder)
+void printCodes(struct MinHeapNode* root, string code){
+    if(!root)
+        return;
 
-    // If this node is a leaf node, it contains a character
-    if (!root->left && !root->right) {
-        huffmanCodes[root->data] = code;
-        cout << root->data << ": " << code << endl;
-    }
-
-    // Traverse left and right subtrees
-    printCodes(root->left, code + "0", huffmanCodes);
-    printCodes(root->right, code + "1", huffmanCodes);
+    // if node is a leaf node then only print the character
+    if(!root->left && !root->right)
+        cout<< root->data<<" : "<< code <<"\n"<<endl;
+    
+    // traverse left and right subtrees with added code "0" and "1" imp. step to get correct code
+    printCodes(root->left, code+"0");
+    printCodes(root->right, code+"1");
 }
 
-// Function to build the Huffman tree and print codes
-void buildHuffmanTree(const string& text) {
-    // Count the frequency of each character
-    unordered_map<char, int> frequencyMap;
-    for (char ch : text) {
-        frequencyMap[ch]++;
+// Build Huffman Tree
+void HuffmanCodes(vector<char> &data, vector<int>& freq){
+    int n = data.size();
+
+    // create a min heap(priority queue)
+    priority_queue<MinHeapNode*, vector<MinHeapNode*>, compare> minHeap;
+
+    for(int i =0;i<n;i++){
+        minHeap.push(new MinHeapNode(data[i], freq[i]));
     }
 
-    // Create a priority queue (min-heap)
-    priority_queue<HuffmanNode*, vector<HuffmanNode*>, Compare> pq;
+    // Iterate until the heap contains only one node (root of the Huffman Tree)
+    while(minHeap.size() != 1){
+        MinHeapNode *left = minHeap.top(); minHeap.pop();
+        MinHeapNode *right = minHeap.top(); minHeap.pop();
 
-    // Create a leaf node for each character and add it to the priority queue
-    for (auto pair : frequencyMap) {
-        pq.push(new HuffmanNode(pair.first, pair.second));
+        MinHeapNode *top = new MinHeapNode('$', left->freq+right->freq);
+
+        top->left = left;
+        top->right = right;
+
+        minHeap.push(top);
     }
 
-    // Iterate until there is only one node left in the priority queue
-    while (pq.size() > 1) {
-        // Remove two nodes of the highest priority (lowest frequency)
-        HuffmanNode* left = pq.top();
-        pq.pop();
-        HuffmanNode* right = pq.top();
-        pq.pop();
-
-        // Create a new internal node with frequency equal to the sum of the two nodes' frequencies
-        HuffmanNode* newNode = new HuffmanNode('\0', left->frequency + right->frequency);
-
-        newNode->left = left;
-        newNode->right = right;
-
-        // Add the new node to the priority queue
-        pq.push(newNode);
-    }
-
-    // The remaining node is the root of the Huffman tree
-    HuffmanNode* root = pq.top();
-
-    // Traverse the Huffman tree and store the codes in a map
-    unordered_map<char, string> huffmanCodes;
-    printCodes(root, "", huffmanCodes);
-
-    // Print the encoded string
-    cout << "\nEncoded string: ";
-    for (char ch : text) {
-        cout << huffmanCodes[ch];
-    }
-    cout << endl;
+    // Print Huffman Codes using the tree built above
+    printCodes(minHeap.top(), "");
 }
 
-int main() {
-    string text;
-    cout << "Enter text to encode using Huffman Encoding: ";
-    getline(cin, text);
 
-    buildHuffmanTree(text);
+int main(){
+
+    vector<char> data = {'a', 'b', 'c', 'd', 'e', 'f'};
+    vector<int> freq = {5, 9, 12, 13, 16, 45};
+
+    cout << "Huffman Codes:\n";
+    HuffmanCodes(data, freq);
 
     return 0;
 }
